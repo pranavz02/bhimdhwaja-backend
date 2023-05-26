be16d15c8f7cbcc28e5d0c65e9e823ad54c95025
pranavz <pranavzinzurde@gmail.com>
2023-05-26 13:28:26 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -23,12 +23,13 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json())
 app.use(cors())
-app.use((req, res) => {
+app.use((req, res, next) => {
 res.header('Access-Control-Allow-Origin', '*');
 res.setHeader("Access-Control-Allow-Credentials", "true");
 res.setHeader("Access-Control-Max-Age", "1800");
 res.setHeader("Access-Control-Allow-Headers", "content-type");
 res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+next();
 });
 
 app.use('/api/products', productRoutes)
