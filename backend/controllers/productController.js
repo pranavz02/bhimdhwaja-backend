5b802e61d94df0753207425baaa6d4124fc19552
pranavz <pranavzinzurde@gmail.com>
2023-06-12 18:05:29 +0530

    fix bugs

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -60,7 +60,7 @@ const createProduct = asyncHandler(async (req, res) => {
     name: 'Sample name',
     price: 0,
     user: req.user._id,
-    image: '',
+    image: '/images/sample.jpg',
     brand: 'Bhimdhwaja',
     category: 'Sample category',
     countInStock: 0,
